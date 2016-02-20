e1fc080bcab8c69fe321a2073808e9d512e9a3a8
Nicklaus  Eckerman <nicklauseckerman@unknown0017f2fc6214.att.net>
2016-02-19 18:01:05 -0600

    Trooper commit.

---
+++ auth.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+good = 12
+bad = 11
+
+GPIO.setup(good, GPIO.OUT)
+GPIO.setup(bad, GPIO.OUT)
+
+def replace_line(file_name, line_num, text):
+    lines = open(file_name, 'r').readlines()
+    lines[line_num] = text
+    out = open(file_name, 'w')
+    out.writelines(lines)
+    out.close()
+
+def go():
+	GPIO.output(bad, 1)
