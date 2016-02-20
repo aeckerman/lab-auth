28563781044b0faf770a040d6d0ab0f16a351ce0
Nicklaus  Eckerman <nicklauseckerman@unknown0017f2fc6214.att.net>
2016-02-19 18:17:56 -0600

    Trooper commit.

--- auth.py
+++ auth.py
@@ -20,11 +20,13 @@ def replace_line(file_name, line_num, text):
     out.close()
 
 def go():
+	GPIO.output(bad, on)
 	password = input('Password: ')
 	if password == 'boyroom':
 		GPIO.output(bad, off)
 		GPIO.output(good, on)
-    GPIO.output(bad, on)
+	else:
+    	GPIO.output(bad, on)
 
 if __name__ == '__main__':
     go()
